4e85b7d2d41a0f93d919948cc7afaac1cbfe16e6
Bell <jacobbrister71@gmail.com>
2022-10-13 01:41:28 -0400

    added get controllers for users

---
+++ controllers/user-controllers.js
@@ -0,0 +1,36 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            });
+    },
+
+    getUserById({ params }, res) {
+        User.findById({ _id: params.id })
+            .populate({
+                path: 'thoughts',
+                select: '-__v'
+            })
+            .populate({
+                path: 'friends',
+                select: '-__v'
+            })
+            .then(dbUserData => {
+                if(!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            })
+    }
+}
